154c0c2e7d98900d12eab36bcfa796bb81fc10f1
NRC7 <n.rodriguez.c7@gmail.com>
2024-11-21 17:19:07 -0300

    Eliminar archivos ya ignorados del seguimiento de Git

--- app/config.py
+++
@@ -1,2 +0,0 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///./local_db.db"
-SQLALCHEMY_TRACK_MODIFICATIONS = False  # Para deshabilitar la advertencia sobre modificaciones
--- run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-
-# Crear la aplicación Flask
-app = create_app()
-
-# Correr la aplicación
-if __name__ == "__main__":
-    app.run(debug=True)
