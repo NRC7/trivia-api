faefe863c07f4a021e60cf94cace8de13aa3f7a4
NRC7 <n.rodriguez.c7@gmail.com>
2024-11-20 22:25:36 -0300

    correcion venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\nrodr\AndroidStudioProjects\xd\venv
+set VIRTUAL_ENV=C:\Users\nrodr\AndroidStudioProjects\Trivia-Api\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
